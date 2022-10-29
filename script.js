a74ba40e9e4cf735e6ea3c8d6bf9537fd76ba143
carter3838 <vigodinvs@gmail.com>
2022-10-29 18:21:32 +0300

    added body of calculate()

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//variables
 let currencyElementOne = document.getElementById('currency-one')
 let currencyElementTwo = document.getElementById('currency-two')
 let amountElementOne = document.getElementById('amount-one')
@@ -5,14 +7,25 @@ let amountElementTwo = document.getElementById('amount-two')
 let rateElement = document.getElementById('rate')
 let swapButton = document.getElementById('swap')
 
+//Eventlisteners
+currencyElementOne.addEventListener('change', calculate)
+amountElementOne.addEventListener('input', calculate)
+currencyElementTwo.addEventListener('change', calculate)
+amountElementTwo.addEventListener('input', calculate)
 
 
 
-
-
-
-
+//fetch exchange rates and updates the DOM
 function calculate() {
+    let currencyOne = currencyElementOne.value
+    let currencyTwo = currencyElementTwo.value
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
+        .then(res => res.json())
+        .then(data => {
+            let rate = data.rates[currencyTwo]
+            rateElement.innerText = `1 ${currencyOne} = ${rate} ${currencyTwo}`
+            amountElementTwo.value = (amountElementOne.value * rate).toFixed(2)
+        })
 
 }
 
