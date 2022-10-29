89a3ecdbb663ec2102ed3112440ae667a18b4376
carter3838 <vigodinvs@gmail.com>
2022-10-29 17:56:29 +0300

    changed code structure, added comments and event listeners

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let currencyElementOne = document.getElementById('currency-one')
 let currencyElementTwo = document.getElementById('currency-two')
 let amountElementOne = document.getElementById('amount-one')
 let amountElementTwo = document.getElementById('amount-two')
-
+let rateElement = document.getElementById('rate')
+let swapButton = document.getElementById('swap')
 
 
 
