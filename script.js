17cee472d3db50591c12edc18561a8d6a605fa8f
carter3838 <vigodinvs@gmail.com>
2022-10-29 17:28:33 +0300

    added variables and a skeleton of a function

---
+++ script.js
@@ -0,0 +1,18 @@
+let currencyElementOne = document.getElementById('currency-one')
+let currencyElementTwo = document.getElementById('currency-two')
+let amountElementOne = document.getElementById('amount-one')
+let amountElementTwo = document.getElementById('amount-two')
+
+
+
+
+
+
+
+
+
+function calculate() {
+
+}
+
+calculate()
